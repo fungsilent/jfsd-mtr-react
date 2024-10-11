54871339d05022933d021ebe4816f54aae677051
fungsilent <funglee162@gmail.com>
2024-10-12 00:37:26 +0800

    translate data to  chinese

--- src/components/TrainsSection.js
+++ src/components/TrainsSection.js
@@ -3,21 +3,27 @@ import TrainCard from './TrainCard'
 
 const TrainsSection = ({ direction, lineCode, dataset }) => {
     if (!lineCode) return null
+    const color = mtrLines[lineCode].color
 
     const destinations = mtrLines[lineCode][direction]
         .map(stationCode => getStationName(lineCode, stationCode))
         .join(' / ')
 
     return (
-        <div className='trains-section'>
-            <p className='title'>往 {destinations} 方向</p>
+        <div
+            className='trains-section'
+            style={{ '--color': color }}
+        >
+            <p className='title'>
+                往 <span>{destinations}</span> 方向
+            </p>
             <div className='trains'>
                 {dataset.length > 0 ? (
                     dataset.map(data => (
                         <TrainCard
                             key={data.name}
                             lineCode={lineCode}
-                            color={mtrLines[lineCode].color}
+                            color={color}
                             {...data}
                         />
                     ))
